013f82780b98b1883082b411e7c9ed1b5d2a1458
Bilal Haroon <bilal.a.haroon@protonmail.com>
2021-09-26 14:23:39 -0500

    responsive login

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,33 @@ import {
 } from "react-router-dom";
 
 import Home from "./Components/Home"
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 function App() {
+
+  const theme = createTheme({
+
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#019000",
+      },
+      secondary: {
+        main: "#F30079"
+      }
+    }
+  })
   return (
+
     <Router>
       <Switch>
         <Route path="/">
-          <Home />
+          <ThemeProvider theme={theme}>
+            <Home />
+          </ThemeProvider>
         </Route>
       </Switch>
     </Router>
+
   );
 }
 
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,37 +1,43 @@
 import React, { FC } from 'react'
 import './Home.css'
 import { ReactComponent as Logo } from './ParrotPen.svg'
-
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
 
 function Home<FC>() {
     return (
 
-        <div>
+        <div className="container">
+            <h1 className="hero-header"> كلام  </h1>
+            <h2 className="hero-subheading">Stay Connected, The Halal Way.</h2>
 
-            <div className="container">
-                <h1 className="hero-header"> كلام  </h1>
-                <h2 className="hero-subheading">Stay Connected, The Halal Way.</h2>
-                {/* <h2 className="hero-subheading"></h2> */}
 
-            </div>
             <LoginSection />
         </div>
     )
 }
 
+const InputButton = styled(Button)({
+    marginTop: '30px',
+    height: '60px',
+    fontSize: '36px',
+    marginBottom: '20px',
+    textTransform: 'capitalize'
+})
 function LoginSection<FC>() {
     return (
-        <div className="login-container">
+        <Paper elevation={6} className="login-container">
             <Logo className="logo" />
-            <form action="">
-                <input type="text" name="username" placeholder="Username or Email" id="" className="input-field" />
-                <input type="text" name="password" placeholder="Password" id="" className="input-field" />
-                <input type="submit" value="Log In" className="input-field primary-button" />
-            </form>
-            <a href="http://example.com">Forgot Password?</a>
+
+            <input type="text" name="username" placeholder="Username or Email" id="" className="input-field" />
+            <input type="password" name="password" placeholder="Password" id="" className="input-field" />
+            <InputButton className="input-field" color="primary" variant="contained">Log in</InputButton>
+
+            <a href="http://example.com" style={{ marginBottom: '10px', fontSize: '18px' }}>Forgot Password?</a>
             <hr style={{ width: '90%', height: 0 }} />
-            <button className="input-field"></button>
-        </div>
+            <InputButton className="input-field" variant="contained" color="secondary">Sign Up</InputButton>
+        </Paper>
     )
 }
 export default Home
